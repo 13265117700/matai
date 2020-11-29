5fad942288d4720e26545e8a177428fc24562836
li-hongfu <304860062@qq.com>
2020-11-29 16:37:25 +0800

    更新登录页、首页样式，编写新页面关注

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,40 +5,75 @@ const app = getApp()
 Page({
   data:{
     current: 1,
-    imageList: [
-        {
-          url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/1.png',
-          mode: "widthFix"
-        },
-        {
-          url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/2.png',
-          mode: "widthFix"
-        },
-        {
-          url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/3.png',
-          mode: "widthFix"
-        },
-        {
-          url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/4.png',
-          mode: "widthFix"
-        },
-        {
-          url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/5.png',
-          mode: "widthFix"
-        }
-    ],
+    imageList: [{
+      url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/1.png',
+      mode: "widthFix"
+    },{
+      url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/2.png',
+      mode: "widthFix"
+    },{
+      url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/3.png',
+      mode: "widthFix"
+    },{
+      url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/4.png',
+      mode: "widthFix"
+    },{
+      url: 'https://activity.vtuzx.com/doc/vtuUI/weapp/swiper/5.png',
+      mode: "widthFix"
+    }],
     cardList:[{
-        url: '/images/index/chuanyuan@3x.png',
-        text: '船源信息'
+      url: '/images/index/chuanyuan@3x.png',
+      text: '船源信息'
     },{
-        url: '/images/index/huoyuan@3x.png',
-        text: '船运货源'
+      url: '/images/index/huoyuan@3x.png',
+      text: '船运货源'
     },{
-        url: '/images/index/cy@3x.png',
-        text: '车运货源'
+      url: '/images/index/cy@3x.png',
+      text: '车运货源'
     },{
-        url: '/images/index/daolu@3x.png',
-        text: '车辆信息'
+      url: '/images/index/daolu@3x.png',
+      text: '车辆信息'
+    }],
+    orderList:[{
+      image: '/images/index/order_img3.png',
+      name: '粤清远货3888',
+      amount: '煤炭  2000吨',
+      begin: '广州黄埔装货港',
+      end: '珠海大湾区目的港',
+      time: '2020-03-26',
+      lowerImages: '/images/index/order-lower_info3.png',
+      lowerInfo: '船已到装货港',
+      lowerSeat: '/images/index/order-lower_seat3.png'
+    },{
+      image: '/images/index/ly@3x.png',
+      name: '粤B.88888',
+      amount: '煤炭  2000吨',
+      begin: '成都武侯区',
+      end: '德阳中江县',
+      time: '2020-08-11',
+      lowerImages: '/images/index/lowerImages.png',
+      lowerInfo: '车已到装货站点',
+      lowerSeat: '/images/index/order-lower_seat3.png'
+    },{
+      image: '/images/index/order_img3.png',
+      name: '粤清远货3888',
+      amount: '煤炭  2000吨',
+      begin: '广州黄埔装货港',
+      end: '珠海大湾区目的港',
+      time: '2020-03-26',
+      lowerImages: '/images/index/order-lower_info3.png',
+      lowerInfo: '船已到装货港',
+      lowerSeat: '/images/index/order-lower_seat3.png'
+    },{
+      image: '/images/index/ly@3x.png',
+      name: '粤B.88888',
+      amount: '煤炭  2000吨',
+      begin: '成都武侯区',
+      end: '德阳中江县',
+      time: '2020-08-11',
+      lowerImages: '/images/index/lowerImages.png',
+      lowerInfo: '车已到装货站点',
+      lowerSeat: '/images/index/order-lower_seat3.png'
     }],
     loading: false
   },
