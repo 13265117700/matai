73edf5698e38ede90ff8899e78121f061764db2e
lihongfu <304860062@qq.com>
2020-12-03 12:21:37 +0800

    认证页面样式

--- pages/my/userIdentit/userIdentit.js
+++ pages/my/userIdentit/userIdentit.js
@@ -3,6 +3,7 @@ Page({
   data: {
     identitId:null,
     ahtcId:null,
+    navbarTitle:'',
     personalForm:[{
       title:'姓名',
       inputType:'text',
@@ -16,15 +17,40 @@ Page({
       inputType:'idcard',
       placeholder:'请输入证件号'
     }],
-    imgJust:[{url:''}],
-    imgThe:[{url:''}]
+    radioTitle:'提交认证',
+    disabled: false,
+    checked: false,
   },
   onLoad: function (options) {
     this.setData({
       identitId:options.identitId,
       ahtcId:options.ahtcId
     })
+    let identitId = this.data.identitId;
+    switch(identitId){
+      case '1':
+        this.setData({
+          navbarTitle:'申请船东认证'
+        })
+        break;
+      case '2':
+        this.setData({
+          navbarTitle:'申请货主认证'
+        })
+        break;
+      case '3':
+        this.setData({
+          navbarTitle:'申请车主认证'
+        })
+        break;
+    }
     console.log(this.data.identitId)
     console.log(this.data.ahtcId)
-  }
+  },
+  handleAnimalChange({ detail = {} }) {
+    this.setData({
+        checked: detail.current
+    });
+    console.log(this.data.checked)
+},
 })
