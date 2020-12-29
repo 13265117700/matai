e627d0d6f3e45d4f9434d0505b176dd2becc2f02
lihongfu <304860062@qq.com>
2020-12-29 17:01:35 +0800

    aaaa

--- models/request/api.js
+++ models/request/api.js
@@ -1,5 +1,8 @@
 const PREFIX  = 'http://8.129.87.113:8005';
+const WebSocket = 'ws://8.129.87.113:8088';
 export default {
+    UserFriendChat:`${WebSocket}/ws`,//websocket连接通信
+
     // 微信登录
     wxLogin: `${PREFIX}/login/code`,
     
@@ -12,11 +15,21 @@ export default {
     UserShipAdd:`${PREFIX}/user/ship/mtShipSave`,//用户添加船舶
     UserShipQuery:`${PREFIX}/user/ship/myFriendsRequestFriends`,//用户船列表查询
     UserShipInfoQuery:`${PREFIX}/user/ship/findByShipId`,//用户船信息查询
+
+    UserMtCargoSave:`${PREFIX}/user/cargo/mtCargoSave`,//用户货源添加
+    UserMtCargoQuery:`${PREFIX}/user/cargo/mtCargoFriends`,//用户货源列表查询
+
+    UserFriendsListL:`${PREFIX}/user/friend/myFriendFriends`,//用户好友列表
+    UserFriendSearch:`${PREFIX}/user/friend/searchFriends`,//用户好友搜索
+    UserFriendRequest:`${PREFIX}/user/friend/friendApplication`,//用户好友申请
+    UserFriendRequestList:`${PREFIX}/user/friend/myFriendsRequestFriends`,//用户好友申请列表
+    UserFriendVerification:`${PREFIX}/user/friend/agreeOrRefuse`,//用户好友验证
+    UserFriendChatMsg:`${PREFIX}/user/mtChatMsg/informationNotSignedByUsers`,//查询用户未签收的信息
+
     
     mtShipownerUpdate:`${PREFIX}/user/information/mtShipownerUpdate`,//船东信息修改
     mtCargoOwnerUpdate:`${PREFIX}/user/information/mtCargoOwnerUpdate`,//货主信息修改
     mtOwnerUpdate:`${PREFIX}/user/information/mtOwnerUpdate`,//车主信息修改
-    UserMtCargoSave:`${PREFIX}/user/cargo/mtCargoSave`,//用户货源添加
     userAppointFriends:`${PREFIX}/user/appoint/appointFriends`,//船东\货主接单查询
 
 
